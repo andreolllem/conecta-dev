8015a235b4d91f05846564dde6443bda6b4b54db
AndreOllem <andreollem@gmail.com>
2020-07-18 17:52:37 -0300

    React-router 6

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import Home from "./pages/Home";
 import { ThemeProvider } from "@material-ui/core/styles";
 import theme from "./theme/index";
+import SignIn from "./pages/Signin/index";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
     return (
         <ThemeProvider theme={theme}>
-            <Home />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/sign-in" element={<SignIn />} />
+                    <Route path="*" element={<h1>Not Found 404!</h1>} />
+                </Routes>
+            </BrowserRouter>
         </ThemeProvider>
     );
 }
